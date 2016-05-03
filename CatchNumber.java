d02c0d0c774025a02f8e5c69c1fe8fedf05a6c9d
qzer9816 <qzer9816@gmail.com>
2016-05-03 18:36:05 +0800

    加註解

--- CatchNumber.java
+++ CatchNumber.java
@@ -2,13 +2,16 @@ import java.math.BigDecimal;
 
 public class CatchNumber{
 	public static double add(double v1,double v2){
-
+	
+	//BigDecimal b1
 	BigDecimal b1 = new
 	BigDecimal(Double.toString(v1));
 
+	//BigDecimal b2
 	BigDecimal b2 = new
 	BigDecimal(Double.toString(v2));
 
+	//Add b1 and b2
 	return b1.add(b2).doubleValue();
 	}
 }
