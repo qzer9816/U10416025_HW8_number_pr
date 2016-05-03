61ad084110fadcc8c5e6fa13484c91879edced9b
qzer9816 <qzer9816@gmail.com>
2016-05-03 18:39:16 +0800

    加註解

--- TestCatchNumber.java
+++ TestCatchNumber.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class TestCatchNumber{
-		public static void main(String[] args){
-			Scanner input = new Scanner(System.in);
-			
-			System.out.println("Enter your number1:");
-			Double v1 = input.nextDouble();
-			
-			System.out.println("Enter your number2:");
-			Double v2 = input.nextDouble();
-			
-			CatchNumber catch1 = new CatchNumber();
-			System.out.println("Total:" + catch1.add(v1,v2));
-		}
+	//Main Method
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		//input v1
+		System.out.println("Enter your number1:");
+		Double v1 = input.nextDouble();
+		
+		//input v2
+		System.out.println("Enter your number2:");
+		Double v2 = input.nextDouble();
+		
+		//create catch object
+		CatchNumber catch1 = new CatchNumber();
+		System.out.println("Total:" + catch1.add(v1,v2));
+	}
 }
